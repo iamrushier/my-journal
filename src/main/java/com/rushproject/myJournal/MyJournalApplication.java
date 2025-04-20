22335caa2ae105b613f59ea21a6f05e10ccfd6d1
iamrushier <rushikeshsurve193@gmail.com>
2025-04-20 11:25:49 +0530

    Initialized journal project

---
+++ src/main/java/com/rushproject/myJournal/MyJournalApplication.java
@@ -0,0 +1,13 @@
+package com.rushproject.myJournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJournalApplication.class, args);
+	}
+
+}
