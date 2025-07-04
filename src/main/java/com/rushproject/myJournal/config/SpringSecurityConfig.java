f768dab75eddee82c8d780a6623d729f9bbd6ab0
iamrushier <rushikeshsurve193@gmail.com>
2025-07-04 13:09:42 +0530

    Used JWT for login

--- src/main/java/com/rushproject/myJournal/config/SpringSecurityConfig.java
+++ src/main/java/com/rushproject/myJournal/config/SpringSecurityConfig.java
@@ -1,11 +1,14 @@
 package com.rushproject.myJournal.config;
 
+import com.rushproject.myJournal.filter.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,26 +16,32 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration // Marks this class as a configuration class.
 @EnableWebSecurity // Enables Spring Security for the app.
 public class SpringSecurityConfig {
     @Autowired
     private UserDetailsService userDetailsService;
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         return http.authorizeHttpRequests(request -> request
-//                        .requestMatchers("/public/**").permitAll()
-//                        .requestMatchers("/journal/**", "/user/**").authenticated()
-//                        .requestMatchers("/admin/**").hasRole("ADMIN")
-//                        .anyRequest().authenticated())
-                        .requestMatchers("/journal/**","/user/**").authenticated()
+                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/journal/**", "/user/**").authenticated()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .anyRequest().permitAll())
-                .httpBasic(Customizer.withDefaults())
+                        .anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Bean
@@ -40,9 +49,9 @@ public class SpringSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
+        return auth.getAuthenticationManager();
     }
 
 }
--- src/main/java/com/rushproject/myJournal/controller/PublicController.java
+++ src/main/java/com/rushproject/myJournal/controller/PublicController.java
@@ -2,22 +2,59 @@ package com.rushproject.myJournal.controller;
 
 import com.rushproject.myJournal.entity.User;
 import com.rushproject.myJournal.service.UserService;
+import com.rushproject.myJournal.utils.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Slf4j
 @RequestMapping("/public")
 public class PublicController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @GetMapping("/health-check")
     public String healthCheck() {
         return "OK";
     }
 
-    @PostMapping("/create-user")
-    public void createUser(@RequestBody User user) {
+    @PostMapping("/signup")
+    public void signUp(@RequestBody User user) {
         userService.saveNewUser(user);
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            user.getUserName(), user.getPassword()
+                    )
+            );
+            UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
+
+            String jwt = jwtUtil.generateToken(userDetails.getUsername());
+            return new ResponseEntity<>(jwt, HttpStatus.OK);
+        } catch (AuthenticationException e) {
+            log.error("Exception occurred while createAUthenticationToken ",e);
+            return new ResponseEntity<>("Incorrect username or password",HttpStatus.BAD_REQUEST);
+        }
+    }
+
 }
